31c5d93143e048b3980cbc18cf2e0dcc8fca6eb2
Josué Nunes <josueemcontato@gmail.com>
2024-07-29 15:46:47 -0300

    feat: Adicionar componente de botão

---
+++ src/components/button/index.tsx
@@ -0,0 +1,13 @@
+import { text } from "stream/consumers";
+
+
+export default function Button({ text, onClick }: { text: string, onClick: () => void }) {
+    return (
+        <button
+            className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300  ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    )
+}
